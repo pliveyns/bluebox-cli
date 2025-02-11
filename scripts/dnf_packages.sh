137dd724798cb81a577c8f369fc4e536505a5bda
Peter Liveyns <peter.liveyns@gmail.com>
2025-02-11 21:57:14 +0100

    Debug dnf_packages.sh

--- scripts/dnf_packages.sh
+++ scripts/dnf_packages.sh
@@ -6,7 +6,7 @@
 echo "--- Installing DNF packages defined in bluebox_dnf.packages --"
 
 dnf update -y
-grep -v '^#' .bluebox_dnf.packages | xargs dnf install -y
+grep -v '^#' ./bluebox_dnf.packages | xargs dnf install -y
 
 echo "---"
 
