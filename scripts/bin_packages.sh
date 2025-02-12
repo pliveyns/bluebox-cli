6453e9c158d9a7ab2b3803d5bdae008e22f704a9
Peter Liveyns <peter.liveyns@gmail.com>
2025-02-12 11:20:17 +0100

    Remove debug info from bin_packages.sh script

--- scripts/bin_packages.sh
+++ scripts/bin_packages.sh
@@ -7,13 +7,10 @@ echo "--- Installing binary packages from download link in bluebox_bin.packages 
 
 binary_packages=/bluebox_bin.packages && \
 while IFS= read -r pkg; do \
-  echo "package is" $pkg; \
   bin=$(echo "$pkg" | cut -d':' -f1 -); \
-  echo "binary is" $bin; \
   url=$(echo "$pkg" | cut -d' ' -f2 -); \
-  echo "url is" $url;\
   echo "Installing: ${bin}" && \
-  curl -v -L $url -o /usr/local/bin/$bin; \
+  curl -L $url -o /usr/local/bin/$bin; \
   chmod +x /usr/local/bin/$bin; \
 done < "$binary_packages" && \
 
