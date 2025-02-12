e75c7d1e27b71503839be2a341ecf8bad8971f9e
Peter Liveyns <peter.liveyns@gmail.com>
2025-02-12 10:04:33 +0100

    Debug bin_packages.sh script

--- scripts/bin_packages.sh
+++ scripts/bin_packages.sh
@@ -7,10 +7,13 @@ echo "--- Installing binary packages from download link in bluebox_bin.packages 
 
 binary_packages=/bluebox_bin.packages && \
 while IFS= read -r pkg; do \
+  echo "package is" $pkg; \
   bin=$(echo "$pkg" | cut -d':' -f1 -); \
+  echo "binary is" $bin; \
   url=$(echo "$pkg" | cut -d' ' -f2 -); \
+  echo "url is" $url;\
   echo "Installing: ${bin}" && \
-  curl -L $url -o /usr/local/bin/$bin; \
+  curl -v -L $url -o /usr/local/bin/$bin; \
   chmod +x /usr/local/bin/$bin; \
 done < "$binary_packages" && \
 
