895ef407bfc516c8a26b49e40bca512cf888c7d5
Peter Liveyns <peter.liveyns@gmail.com>
2025-02-12 20:17:17 +0100

    Refactor bin_packages.sh script

--- scripts/bin_packages.sh
+++ scripts/bin_packages.sh
@@ -3,16 +3,16 @@
 # Install binary packages from bluebox_bin.packages
 #
 
-echo "--- Installing binary packages from download link in bluebox_bin.packages --" && \
+echo "--- Installing binary packages from download link in bluebox_bin.packages --"
 
-binary_packages=/bluebox_bin.packages && \
-while IFS= read -r pkg; do \
-  bin=$(echo "$pkg" | cut -d':' -f1 -); \
-  url=$(echo "$pkg" | cut -d' ' -f2 -); \
-  echo "Installing: ${bin}" && \
-  curl -L $url -o /usr/local/bin/$bin; \
-  chmod +x /usr/local/bin/$bin; \
-done < "$binary_packages" && \
+binary_packages=/bluebox_bin.packages
+while IFS= read -r pkg; do
+  bin=$(echo "$pkg" | cut -d':' -f1 -)
+  url=$(echo "$pkg" | cut -d' ' -f2 -)
+  echo "Installing: ${bin}"
+  curl -L $url -o /usr/local/bin/$bin
+  chmod +x /usr/local/bin/$bin
+done < "$binary_packages"
 
 echo "---"
 
