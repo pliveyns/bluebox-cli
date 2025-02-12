be24fa739d1ba09f11113f1fd2a5ee5bd1abbcdf
Peter Liveyns <peter.liveyns@gmail.com>
2025-02-12 19:46:21 +0100

    Add script for install of rpm packages from download

--- scripts/rpm_packages.sh
+++ scripts/rpm_packages.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 #
-# Install rpm packages from recipe.yml
+# Install rpm packages from bluebox_rpm.packages
 #
 
-echo "--- Installing RPM packages from url defined in recipe.yml --"
+echo "--- Installing RPM packages from url defined in bluebox_rpm.packages ---"
 
-rpm_urls=$(yq '.rpm[]' < /tmp/config/recipe.yml | sed -e "s/: /\&/")
-for pkg in $rpm_urls; do
-    bin=$(echo $pkg | cut -d'&' -f1 -)
-    url=$(echo $pkg | cut -d'&' -f2 -)
-    echo "Installing: ${bin}"
-    dnf install -y $url
+rpm_packages=/bluebox_rpm.packages && \
+while IFS= read -r pkg; do
+  bin=$(echo"$pkg" | cut -d':' -f1 -)
+  url=$(echo "$pkg" | cut -d ' ' -f2 -)
+  echo "Installing: ${bin}"
+  dnf install -y $url
 done
 
 echo "---"
