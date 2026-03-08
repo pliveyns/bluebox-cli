06301e29eef6b5dffa13f5ea1d31638f5dc6522d
Peter Liveyns <peter.liveyns@gmail.com>
2026-03-08 20:44:14 +0100

    Debug yazi install script

--- scripts/yazi.sh
+++ scripts/yazi.sh
@@ -7,5 +7,6 @@ echo "--- Installing yazi from copr repo"
 
 dnf copr enable -y lihaohong/yazi
 dnf --enablerepo copr:copr.fedorainfracloud.org:lihaohong:yazi install -y yazi
+dnf install -y ffmpeg-free
 
 echo "---"
